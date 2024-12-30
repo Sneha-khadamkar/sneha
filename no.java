010be5c0e9077ef172fc120cf542064cd50db705
Sneha-khadamkar <sneha.khadamkar_24uds@sanjivani.edu.in>
2024-12-30 12:19:50 +0530

    Update no.java

--- no.java
+++ no.java
@@ -2,8 +2,10 @@ class no
   {
     public static void main(String args[])
     {
-      for(int i=0;i<=10;i++)
-        }
+      for(int i=0;i<=10;i++){
+        
         System.out.println(i);
+      }
+    }
 }
       
